fb948bc865bf98d0cf9bc93bc37a36e65c272ea5
shadow-eljan <eljanrai25@gmail.com>
2025-12-18 12:17:55 +0545

    Converting lowercase word to uppercase

---
+++ src/Practical/LowerToUpper.java
@@ -0,0 +1,13 @@
+package Practical;
+
+import java.util.Scanner;
+
+public class LowerToUpper {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String name = s.nextLine();
+        System.out.println(name + " converted to: "+ name.toUpperCase());
+    }
+}
