12f7a707e091c48c82d70fee04ed3f60a98bff0b
shadow-eljan <eljanrai25@gmail.com>
2025-12-20 20:07:51 +0545

    Sum of Array

---
+++ src/Practical/SumOfArray.java
@@ -0,0 +1,19 @@
+package Practical;
+
+import java.util.Scanner;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] arr = new double[100];
+        double sum = 0;
+        System.out.println("Enter 10 values: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextDouble();
+        }
+        for (double eachValue : arr) {
+            sum += eachValue;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
