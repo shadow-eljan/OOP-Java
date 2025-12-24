76c5d750db96e6727c64873c4a26a294fec80b5d
shadow-eljan <eljanrai25@gmail.com>
2025-12-24 19:47:08 +0545

    Sub class object accessing

--- src/OOP/Father.java
+++ src/OOP/Father.java
@@ -4,4 +4,9 @@ public class Father {
     void father(){
         System.out.println("I am father");
     }
+
+    public static void main(String[] args) {
+        Father f = new Son();
+        f.father();
+    }
 }
