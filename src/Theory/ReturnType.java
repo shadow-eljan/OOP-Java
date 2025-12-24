c4a82967d4db259c2f67f36fa64b3ea166193ecf
shadow-eljan <eljanrai25@gmail.com>
2025-12-24 19:55:37 +0545

    static vs return vs void method

--- src/Theory/ReturnType.java
+++ src/Theory/ReturnType.java
@@ -1,14 +1,20 @@
 package Theory;
 
 public class ReturnType {
-    public String hello(){
+    String hello(){
         return "HELLO.";
     }
 
     static String world(){
         return "WORLD.";
     }
+    void hi(){
+        System.out.println("HI");
+    }
     public static void main(String[] args) {
         System.out.println(world());
+        ReturnType r = new ReturnType();
+        r.hi();
+        System.out.println(r.hello());
     }
 }
