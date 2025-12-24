4efe6713f650468a83b141c0753ea030d52bb341
shadow-eljan <eljanrai25@gmail.com>
2025-12-24 19:51:57 +0545

    iib vs static block

--- src/Theory/iib.java
+++ src/Theory/iib.java
@@ -4,6 +4,9 @@ public class iib {
     static{
         System.out.println("Runs before the constructor");
     };
+    {
+        System.out.println("Runs before the constructor, but after upper print.");
+    }
     iib(){
         System.out.println("HEllo.");
     }
